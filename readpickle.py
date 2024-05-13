b7eec0349dde8d48b8cdc7f97ae09661b7655ca4
Carter <ac02351@condor.eps.surrey.ac.uk>
2024-05-13 20:25:27 +0100

    update file path

--- readpickle.py
+++ readpickle.py
@@ -1,6 +1,5 @@
 import pickle
 
-# pickle_file_path = '/vol/research/DeepFakeDet/rPPG-Toolbox/runs/exp/logs/PhysNet_Face2Face_ADAM_LR=0.001_LRReducer_16_frames_0.5binary_0.5rPPG_scaling_0.001_0.0001_LR_multi_model_epoch11_test/Face2Face/saved_test_outputs/PhysNet_NeuralTextures_ADAM_LR=0.001_LRReducer_16_frames_0.5binary_0.5rPPG_scaling_0.001_0.0001_LR_multi_model_Epoch11_Face2Face_outputs.pickle'
 pickle_file_path="/vol/research/DeepFakeDet/rPPG-Toolbox/runs/exp/logs/PhysNet_NeuralTextures_ADAM_LR=0.001_LRReducer_16_frames_0.3binary_0.7rPPG_scaling_multi_model/NeuralTextures/saved_test_outputs/PhysNet_NeuralTextures_ADAM_LR=0.001_LRReducer_16_frames_0.3binary_0.7rPPG_scaling_multi_model_outputs.pickle"
 
 def load_pickle_data(file_path):
